885200f8254c990a046b1ffd29347a7f5cc91f29
Mark Cormack <markcormack@gmail.com>
2020-05-21 23:36:54 +0100

    Initial Commit

---
+++ MC_OnboardingKit/MC_OnboardingKit.h
@@ -0,0 +1,19 @@
+//
+//  MC_OnboardingKit.h
+//  MC_OnboardingKit
+//
+//  Created by Mark Cormack on 21/05/2020.
+//  Copyright © 2020 Mark Cormack. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MC_OnboardingKit.
+FOUNDATION_EXPORT double MC_OnboardingKitVersionNumber;
+
+//! Project version string for MC_OnboardingKit.
+FOUNDATION_EXPORT const unsigned char MC_OnboardingKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MC_OnboardingKit/PublicHeader.h>
+
+
